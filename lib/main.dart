964dc988930e1b27cd0a7e36611d28ad8abf0d8a
= <=>
2021-02-17 19:40:22 +0700

    Latihan Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,28 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // Add your onPressed code here!
-          },
-          child: Icon(Icons.thumb_up),
-          backgroundColor: Colors.pink,
+        appBar: AppBar(
+          title: Text('Sample Code'),
         ),
+        body: Center(
+          child: Text('You have pressed the button $_count times.'),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            height: 50.0,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => 0,
+          tooltip: 'Increment Counter',
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
