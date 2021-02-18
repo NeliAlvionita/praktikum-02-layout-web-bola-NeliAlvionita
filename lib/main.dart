91a300ce83fcfd5b2e55450fe1e8535a202259a0
= <=>
2021-02-18 21:33:43 +0700

    Tugas Masih Error

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,33 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Contoh TextField")),
-        body: TextField(
-          obscureText: false,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: 'Nama',
+        appBar: AppBar(title: Text("My App"), backgroundColor: Colors.red),
+        body: 
+          Row(
+             children: <Widget>[
+                Container(
+                  color: Colors.white,
+                  alignment: Alignment.centerLeft,
+                     child: Text("BERITA TERBARU",
+                     style: TextStyle(fontSize: 30, color: Colors.black)),
+                ),
+                Container(
+                  color: Colors.white,
+                  alignment: Alignment.centerRight,
+                    child: Text("PERTANDINGAN HARI INI",
+                    style: TextStyle(fontSize: 30, color: Colors.black)),
+                ),
+             ],
           ),
-        ),
+          Column(
+                children: <Widget> [
+                Image.network(''),
+                Text(
+                  'Transfer', style: TextStyle(fontSize: 12, color: Colors.black, backgroundColor: Colors.red)
+                ),
+              ],
+          ),
+
       ),
     );
   }
